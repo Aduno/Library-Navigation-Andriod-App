5ff4632d7fb701869dca161747d15023dce1ea2e
Aduno <sanp100d@gmail.com>
2021-10-05 22:00:15 -0400

    added click functionality

--- app/src/main/java/com/example/wayfinding/MainActivity.java
+++ app/src/main/java/com/example/wayfinding/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.wayfinding;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+    //Sends the user to navigation/home page
+    public void goToNav(View v){
+        Intent intent = new Intent(MainActivity.this, NavigationScreen.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+    }
+
+    //Sends the user to login page
+    public void goToSignin(View v){
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_right);
     }
 }
