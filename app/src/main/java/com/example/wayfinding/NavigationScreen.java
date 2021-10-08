2262037722dd3144246c816098959f3e8c2e6064
Aduno <sanp100d@gmail.com>
2021-10-07 21:12:44 -0400

    previous files

--- app/src/main/java/com/example/wayfinding/NavigationScreen.java
+++ app/src/main/java/com/example/wayfinding/NavigationScreen.java
@@ -10,5 +10,6 @@ public class NavigationScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation_screen);
+
     }
 }
