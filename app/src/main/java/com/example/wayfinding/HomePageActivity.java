1357c61436bb98383642aaf806b0c0ab56bcc6b9
Aduno <sanp100d@gmail.com>
2021-10-10 18:57:48 -0400

    added fonts

--- app/src/main/java/com/example/wayfinding/HomePageActivity.java
+++ app/src/main/java/com/example/wayfinding/HomePageActivity.java
@@ -2,9 +2,13 @@ package com.example.wayfinding;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -24,7 +28,14 @@ public class HomePageActivity extends AppCompatActivity {
         //Need to populate the list with announcement information
         items = new ArrayList<>();
         getAnnouncements(items);
-        adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, items);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent){
+                TextView item = (TextView) super.getView(position,convertView,parent);
+                item.setTypeface(Typeface.createFromAsset(getAssets(),"amethysta.ttf"));
+                return item;
+            }
+        };
         announcementList.setAdapter(adapter);
     }
 
@@ -35,5 +46,9 @@ public class HomePageActivity extends AppCompatActivity {
     public void getAnnouncements(ArrayList<String> items){
         //need to connect to db  and retrieve info
         items.add("The library will be closed from 6-8PM on October 16 for temporary repairs on the elevator");
+        items.add("Acc 2 The library will be closed from 6-8PM on October 16 for temporary repairs on the elevator");
+        items.add("Acc 4 The library will be closed from 6-8PM on October 16 for temporary repairs on the elevator");
+        items.add("Please note that you will need to be vaccinated to be on campus. You will need to present you student card and scan it at the entrance to enter the Morisset library");
+        items.add("During reading week, the operation hours of the library will be changed.\nThe hours will be from 7am-5pm on Mon-Fri, and 10am-5pm on the weekends\nThank you for using the Morriset library");
     }
 }
