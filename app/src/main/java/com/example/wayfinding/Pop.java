ef1473675ccef2e5a829936e07c5029b3e732175
Aymane <aay36740@gmail.com>
2021-11-14 19:02:03 -0500

    this is the add_announcement branch :)

--- app/src/main/java/com/example/wayfinding/Pop.java
+++ app/src/main/java/com/example/wayfinding/Pop.java
@@ -2,14 +2,28 @@ package com.example.wayfinding;
 import  android.app.Activity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
 import android.view.Window;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.example.wayfinding.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class Pop extends Activity{
-    EditText inputText;
+
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +35,51 @@ public class Pop extends Activity{
         int width = dm.widthPixels;
         int height = dm.heightPixels;
 
-        getWindow().setLayout((int)(width*.85),(int)(height*.74));
-        inputText= (EditText) findViewById(R.id.text_input);
+        getWindow().setLayout((int)(width*.85),(int)(height*.80));
+
     }
+
+    public void onclick(View view) {
+        EditText text_input1;
+
+        TextView textView;
+        textView = findViewById(R.id.textView4);
+        text_input1 = findViewById(R.id.textView3);
+//        textView.setText("Announcement published successfully.");
+
+        Toast.makeText(this, "announcement", Toast.LENGTH_SHORT).show();
+        StringRequest req = new StringRequest(Request.Method.POST, "192.168.2.12:8080/add_announcement.php", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    String success = jsonObject.getString("success");
+                    if (success.equals("1"))
+                        textView.setText("Announcement published successfully.");
+                    else
+                        textView.setText("Announcement published unsuccessfully.");
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "error" + error.getMessage(), Toast.LENGTH_LONG).show();
+
+            }
+        })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("announcement", text_input1.getText().toString());
+                return params;
+            }
+
+        };
+    }
+
 }
