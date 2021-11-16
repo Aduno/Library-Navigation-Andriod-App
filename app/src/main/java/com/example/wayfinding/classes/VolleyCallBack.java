7cea8fa4ed4146745a68a5d36d959b4fe58db874
Aduno <sanp100d@gmail.com>
2021-11-15 22:08:06 -0500

    connected announcement list to database

---
+++ app/src/main/java/com/example/wayfinding/classes/VolleyCallBack.java
@@ -0,0 +1,7 @@
+// From https://stackoverflow.com/questions/49342841/android-wait-for-volley-response-for-continue
+
+package com.example.wayfinding.classes;
+
+public interface VolleyCallBack {
+    void onSuccess();
+}
